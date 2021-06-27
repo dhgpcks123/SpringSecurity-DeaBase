582fc11659422505887230a0ebc82c695c19c574
hyechan <dhgpcks@gmail.com>
2021-06-27 23:22:46 +0900

    mustache를 활용해서 index페이지 띄우기

--- src/main/java/com/dev/security/controller/IndexController.java
+++ src/main/java/com/dev/security/controller/IndexController.java
@@ -9,7 +9,9 @@ public class IndexController {
     @GetMapping({"","/"})
     public String index() {
         // 머스테치 기본폴더 src/main/resources/
-        // 뷰리졸버 설정 : templates (prefix), .mustache (suffix)
+        // 뷰리졸버 설정 : templates (prefix), .mustache (suffix) 설정 생략가능
+        // prefix, suffix설정은 WebMvcConfig에서 해줬음.
         return "index";
+        // src/main/resources/templates/index.mustache
     }
 }
