6b5dab4010bd8d27fa21866a20dbd66911f983b7
hyechan <dhgpcks@gmail.com>
2021-06-28 20:12:16 +0900

    컨트롤러 추가

--- src/main/java/com/dev/security/controller/IndexController.java
+++ src/main/java/com/dev/security/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.dev.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -13,5 +14,35 @@ public class IndexController {
         // prefix, suffix설정은 WebMvcConfig에서 해줬음.
         return "index";
         // src/main/resources/templates/index.mustache
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료 됨";
     }
 }
